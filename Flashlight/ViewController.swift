64ed8280439c11cfe4022dcdaeac028510fefa77
Emily Kirk <ekirk@emilys-macbook-air-73.local>
2019-04-03 11:13:05 -0500

    flashlight

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flashlightView: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onSwitchChange(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            flashlightView.backgroundColor = .white
+            imageView.image = UIImage(named: "Rubber Duck")
+        } else {
+            flashlightView.backgroundColor = .black
+            imageView.image = UIImage(named: "Rubber Duck II")
+        }
+    }
 }
+
 
